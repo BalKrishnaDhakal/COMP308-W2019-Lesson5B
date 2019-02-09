73eb023f1bc2e70ae434968ed08b46a6b92e6884
Bal krishna Dhakal <bdhakal1@my.centennialcollege.ca>
2019-02-09 16:13:02 -0500

    added delete function

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -1,11 +1,13 @@
-// IIFE -- Immediately Invoked Function Expression
-(function(){
-
-    function Start() {
-        console.log(`%c App Started...`, 
+function Start() {
+    console.log(`%c App Started...`,
         "font-size: 20px; color: blue; font-weight: bold");
-    }
-    
-    window.addEventListener("load", Start);
 
-})();
+    $(".btn-danger").click(function(event) {
+        if (!confirm("Are you sure???")) {
+            event.preventDefault();
+            window.location.assign("/contact-list");
+        }
+    });
+}
+
+window.addEventListener("load", Start);
